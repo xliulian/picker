807619f2fd9734534ea8e77141f934ea26b4f959
Ulion <ulion2002@gmail.com>
2023-01-31 07:24:06 +0800

    gitpkg

---
+++ lib/panels/QuarterPanel/index.d.ts
@@ -0,0 +1,4 @@
+import type { PanelSharedProps } from '../../interface';
+export type QuarterPanelProps<DateType> = {} & PanelSharedProps<DateType>;
+declare function QuarterPanel<DateType>(props: QuarterPanelProps<DateType>): JSX.Element;
+export default QuarterPanel;
